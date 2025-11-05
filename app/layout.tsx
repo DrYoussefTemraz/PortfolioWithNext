cca56fcd58ea2f07b63f02675d84ab79739c2a66
DrYoussefTemraz <youseftahertemraz@gmail.com>
2025-11-05 23:39:45 +0200

    install grid bg and refine with tailwind classes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Roboto_Mono } from "next/font/google";
+import { ThemeProvider } from "@/app/provider"
+
 import "./globals.css";
 
 const geistSans = Inter({
@@ -23,11 +25,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+       <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
       </body>
     </html>
   );
