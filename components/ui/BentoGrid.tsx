117beb8675636f331a6901aed7776a63df42fac9
DrYoussefTemraz <youseftahertemraz@gmail.com>
2025-11-07 00:45:09 +0200

    special customizing of the title and desc int bento grid items depending on the id

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import { BackgroundGradientAnimation } from "./GradientBg";
 
 export const BentoGrid = ({
   className,
@@ -47,15 +48,31 @@ export const BentoGridItem = ({
         className,
       )}
     >
-      <div className="transition duration-200 group-hover/bento:translate-x-2">
+      <div className={`${id === 6 && 'flex justify-center h-full'}`}>
+        <div className="w-full h-full absolute">
+          {img && (<img src={img} alt={img} className={cn(imgClassName, 'object-cover object-center')} />)}
+        </div>
+        <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
+          {spareImg && (<img src={spareImg} alt={spareImg} className='object-cover object-center' />)}
 
-        <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
-          {title}
         </div>
-        <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
-          {description}
+        {id === 6 && (
+          <BackgroundGradientAnimation>
+            <div className="absolute z-50 flex items-center justify-center text-white font-bold " />
+
+          </BackgroundGradientAnimation>
+        )}
+        <div className={cn(titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10')}>
+          <div className="font-sans text-sm font-extralight text-[#c1c2d3] md:text-xs lg:text-base z-20">
+            {description}
+          </div>
+          <div className=" font-sans font-bold text-lg lg:text-3xl max-w-96 z-20">
+            {title}
+          </div>
         </div>
+
       </div>
+
     </div>
   );
 };
